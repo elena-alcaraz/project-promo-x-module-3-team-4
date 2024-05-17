8110b06532caadeb9525b9aea9a680ce275d7ee5
LuciaLacort <lacortlucia@gmail.com>
2024-05-17 13:32:45 +0200

    merge

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,12 @@
 import CardPreview from "./CardPreview";
+import defaultAvatar from '../images/ebook-example.jpg';
 
 const Preview = (props) => {
+  const avatar = props.userData.image === '' ? defaultAvatar : props.userData.image;
   return (
     <section className="preview">
       <div className="projectImage"
-      style={{ backgroundImage: `url(${props.userData.image})` }} ></div>
+      style={{ backgroundImage: `url(${avatar})` }} ></div>
       <CardPreview userData= {props.userData} />
     </section>
   )
