8939da04eba66c06933c0e0511b4028c763fea4a
aidanar <aidanarfer@outlook.es>
2024-05-20 13:58:04 +0200

    commit changes

---
+++ src/components/Fetch.js
@@ -0,0 +1,15 @@
+const fetch = () => {
+    fetch("https://dev.adalab.es/api/projectCard"), {
+        method: 'POST',
+        body: JSON.stringify(cardData),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }
+        .then((response) => response.json())
+        .then((responseData) => {
+            
+      });
+  };
+
+ 
